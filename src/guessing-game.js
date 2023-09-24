38ceae1edd8ac90134f8dc8cd82ea26d161330ae
tatyana <tilkka@mail.ru>
2023-09-25 00:08:50 +0300

    feat: solution of game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    
+    min;
+    max;
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.myGuess = Math.ceil((this.min + this.max)/2);
+        return this.myGuess;
     }
 
     lower() {
-
+        this.max = this.myGuess;
     }
 
     greater() {
-
+        this.min = this.myGuess;
     }
 }
 
 module.exports = GuessingGame;
+
